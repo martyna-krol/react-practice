de3ddfeea5f69769b624293b0fcd23758157ee2b
Martyna Król <martyna.maria.krol@gmail.com>
2022-06-16 17:46:35 +0200

    11.5 Column, Container finished

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,7 +6,9 @@ const SearchForm = () => {
     return (
         <form className={styles.SearchForm}>
             <TextInput placeholder='Search…' />
-            <Button text='Search' />
+            <Button>
+                <span className="fa fa-search" />
+            </Button>
         </form>
     );
   };
