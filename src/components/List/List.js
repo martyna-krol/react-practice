51916a75fd026043e00be4f1581a513dc850343e
Martyna Król <martyna.maria.krol@gmail.com>
2022-06-17 14:38:45 +0200

    ColumnForm finished

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,30 +1,32 @@
 import styles from './List.module.scss';
-import Column from '../Column/Column';
+import Column from './../Column/Column';
+import ColumnForm from './../ColumnForm/ColumnForm';
 import { useState } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
-
     const [columns, setColumns] = useState([
         { id: 1, title: 'Books', icon: 'book' },
         { id: 2, title: 'Movies', icon: 'film' },
         { id: 3, title: 'Games', icon: 'gamepad' }
     ]);
-  
-    /* setTimeout(() => {
-        setColumns([...columns, { id: 4, title: 'Test column'}]);
-      }, 2000); */
 
-    return (
-        <div className={styles.List}>
-            <header className={styles.header}>
-                <h2 className={styles.title}>Things to do<span>soon</span></h2>
-            </header>
-            <p className={styles.description}>Interesting things I want to check out!</p>
-            <section className={styles.columns}>
-                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
-            </section>
-        </div>
-    );
-  };
+	const addColumn = newColumn => {
+		setColumns([...columns, { id: shortid(), title: newColumn.title }]);
+	};
 
-  export default List;
+	return (
+		<div className={styles.list}>
+			<header className={styles.header}>
+				<h2 className={styles.title}>Things to do<span>soon!</span></h2>
+			</header>
+			<p className={styles.description}>Interesting things I want to check out</p>
+			<section className={styles.columns}>
+				{columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
+			</section>
+			<ColumnForm action={addColumn} />
+		</div>
+	);
+};
+
+export default List;
