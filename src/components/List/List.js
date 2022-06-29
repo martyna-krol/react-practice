5ac090dec88d677431484f07d6261e59658654bd
Martyna Król <martyna.maria.krol@gmail.com>
2022-06-29 21:21:18 +0200

    13.2 Redux

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,11 @@
 import styles from './List.module.scss';
 import Column from './../Column/Column';
 import ColumnForm from './../ColumnForm/ColumnForm';
+import { useSelector } from 'react-redux';
 import { useState } from 'react';
-import shortid from 'shortid';
 
 const List = () => {
-    const [columns, setColumns] = useState([
+    {/*const [columns, setColumns] = useState([
 		{
 			id: 1,
 			title: 'Books',
@@ -49,20 +49,26 @@ const List = () => {
 	
 		setColumns(columnsUpdated);
 	
-	};
+	};*/}
+
+	const columns = useSelector(state => state.columns);
 
 	return (
 		<div className={styles.list}>
-			<header className={styles.header}>
-				<h2 className={styles.title}>Things to do<span>soon!</span></h2>
-			</header>
-			<p className={styles.description}>Interesting things I want to check out</p>
-			<section className={styles.columns}>
-				{columns.map(column => <Column key={column.id} id={column.id} title={column.title} icon={column.icon} cards={column.cards} action={addCard} />)}
-			</section>
-			<ColumnForm action={addColumn} />
+		  <header className={styles.header}>
+			<h2 className={styles.title}>Things to do<span>soon!</span></h2>
+		  </header>
+		  <p className={styles.description}>Interesting things I want to check out</p>
+		  <section className={styles.columns}>
+			{columns.map(column =>
+			  <Column
+				key={column.id}
+				{...column}  />
+			)}
+		  </section>
+		  <ColumnForm />
 		</div>
-	);
+	  );
 };
 
 export default List;
