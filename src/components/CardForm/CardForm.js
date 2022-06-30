ca85c64d51b6be3e9447dfa5e5c5999befe00e78
Martyna Król <martyna.maria.krol@gmail.com>
2022-06-30 21:28:23 +0200

    13.3 reducer and dispatch

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,15 +1,16 @@
-
-
 import styles from './CardForm.module.scss';
 import { useState } from 'react';
 import Button from './../Button/Button';
 import TextInput from './../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
 
 const CardForm = props => {
+    const dispatch = useDispatch();
     const [title, setTitle] = useState('');
+    const columnId = props.columnId;
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title }, props.columnId);
+        dispatch({ type: 'ADD_CARD', payload: { title , columnId } });
         setTitle('');
     };
 
