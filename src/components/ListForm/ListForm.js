4fb6829effbde34b91d259c929c2ee4634e3f428
Martyna Król <martyna.maria.krol@gmail.com>
2022-07-06 20:53:49 +0200

    14.3 List of lists

---
+++ src/components/ListForm/ListForm.js
@@ -0,0 +1,32 @@
+import styles from './ListForm.module.scss';
+import { useState } from 'react';
+import Button from './../Button/Button';
+import TextInput from './../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
+import { addList } from '../../redux/store';
+
+
+const ListForm = props => {
+    const dispatch = useDispatch();
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const listId = props.listId;
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(addList({ title, description, listId }));
+        setTitle('');
+        setDescription('');
+    };
+
+	return (
+        <form className={styles.columnForm} onSubmit={handleSubmit}>
+            <label for="title">Title:</label>
+            <TextInput value={title} onChange={e => setTitle(e.target.value)} />
+            <label for="description">Description:</label>
+            <TextInput value={description} onChange={e => setDescription(e.target.value)} />
+            <Button>Add list</Button>
+        </form>
+	);
+};
+
+export default ListForm;
